fa3b43095e21d29548940bbd7cfe6cde6b1664a2
rodrigonoll <noll@192.168.2.103>
2015-04-12 16:27:52 -0300

    Atualzado script SQL

---
+++ src/resources/import.sql
@@ -0,0 +1,18 @@
+insert into usuario(id, email, nome, senha, sobrenome) values (1000, '123@123.123', '123', 'f5bb0c8de146c67b44babbf4e6584cc0','123'); 
+
+insert into medicamento (id, nome) values (1001, 'Paracetamol');
+insert into medicamento (id, nome) values (1002, 'Benegripe');
+
+insert into tipoalergia (id, descricao) values (1, 'Alimento');
+insert into tipoalergia (id, descricao) values (2, 'Ambiental');
+insert into tipoalergia (id, descricao) values (3, 'Animal');
+insert into tipoalergia (id, descricao) values (4, 'Medicação');
+insert into tipoalergia (id, descricao) values (5, 'Planta');
+
+insert into medicamentousuario (ID, DATAFIMTRATAMENTO,DATAINICIOTRATAMENTO, DESCRICAO, DOSAGEM, FREQUENCIA, NOTA, SITUACAO, MEDICAMENTO_ID, USUARIO_ID) VALUES (1000, SYSDATE, SYSDATE, 'DESCRICAO', 'DOSAGEM', 'FREQUENCIA', 'NOTA', 'SITUACAO', 1001, 1000);
+insert into medicamentousuario (ID, DATAFIMTRATAMENTO,DATAINICIOTRATAMENTO, DESCRICAO, DOSAGEM, FREQUENCIA, NOTA, SITUACAO, MEDICAMENTO_ID, USUARIO_ID) VALUES (1001, SYSDATE, SYSDATE, 'DESCRICAO', 'DOSAGEM', 'FREQUENCIA', 'NOTA', 'SITUACAO', 1002, 1000);
+
+
+insert into AlergiaUsuario (dataPrimeiraOcorrencia, MEDICAMENTOUSUARIO_ID, reacao, tipoAlergia_id, USUARIO_ID, id) values (sysdate, 1000, 'Reacao', 1, 1000, 1000);
+
+insert into AlergiaUsuario (dataPrimeiraOcorrencia, MEDICAMENTOUSUARIO_ID, reacao, tipoAlergia_id, USUARIO_ID, id) values (sysdate, 1001, 'Reacao', 1, 1000, 1001);
