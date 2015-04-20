10c55c7ab4ffa32f3d89f002a2634fd8860ce204
pablodiehl <pablodiehl@ymail.com>
2015-04-20 18:47:10 -0300

    adição da classe "PressaoUsuarioDAO.java"

---
+++ src/br/edu/ifrs/canoas/tads/lds/model/dao/PressaoUsuarioDAO.java
@@ -0,0 +1,24 @@
+package br.edu.ifrs.canoas.tads.lds.model.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+import br.edu.ifrs.canoas.tads.lds.bean.PressaoArterial;
+import br.edu.ifrs.canoas.tads.lds.bean.PressaoUsuario;
+import br.edu.ifrs.canoas.tads.lds.bean.Usuario;
+
+@Stateless
+public class PressaoUsuarioDAO extends BaseDAO< PressaoUsuario, Long>{
+	private static final long serialVersionUID = -3230082070333146918L;
+	
+	@SuppressWarnings("unchecked")
+	public List<PressaoArterial> buscaPressaoPorUsuario(Usuario usuario){
+		return em.createQuery(
+		         "SELECT pu.pressaoArterial " 
+		         + "FROM PressaoUsuario pu "
+		         + "WHERE pu.usuario.id = :usuario ")
+		         .setParameter("usuario", usuario.getId())
+		         .getResultList();
+	}
+}
