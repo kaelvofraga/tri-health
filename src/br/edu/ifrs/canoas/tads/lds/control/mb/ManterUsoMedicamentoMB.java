100a805c7b308ad82e9ffc181c58d3b23b150ae1
Alisson <Alisson@200.17.89.133>
2015-04-30 08:48:52 -0300

    Atualização das classes de cadastro, criação view Listar do caso de Uso de Medicamentos.

--- src/br/edu/ifrs/canoas/tads/lds/control/mb/ManterUsoMedicamentoMB.java
+++ src/br/edu/ifrs/canoas/tads/lds/control/mb/ManterUsoMedicamentoMB.java
@@ -9,6 +9,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.edu.ifrs.canoas.tads.lds.bean.Medicamento;
 import br.edu.ifrs.canoas.tads.lds.bean.MedicamentoUsuario;
 import br.edu.ifrs.canoas.tads.lds.control.service.ManterUsoMedicamentoService;
 
@@ -16,7 +17,8 @@ import br.edu.ifrs.canoas.tads.lds.control.service.ManterUsoMedicamentoService;
 @RequestScoped
 public class ManterUsoMedicamentoMB implements Serializable {
 
-
+	private static final String URL_LISTAR_USO_MEDICAMENTOS = "#";
+	private static final String URL_MANTER_USO_MEDICAMENTOS = "/private/pages/manterPerfilEmergencia.jsf";
 	private static final long serialVersionUID = 8840982087710515671L;
 
 	@Inject
@@ -28,12 +30,13 @@ public class ManterUsoMedicamentoMB implements Serializable {
 	private ManterUsoMedicamentoService medicamentoService;
 	
 	
-	private List<MedicamentoUsuario> medicamentos;
+	private List<MedicamentoUsuario> medicamentosLista;
 	
+	private List<Medicamento> medicamentos;
 	
 	public void inicializa() {
 		medicamentoUsuario = new MedicamentoUsuario();
-		medicamentos = new ArrayList();
+		medicamentosLista = new ArrayList<>();
 	}
 	
 	
@@ -44,7 +47,7 @@ public class ManterUsoMedicamentoMB implements Serializable {
 	
 	public void salvaMedicamento(){
 		medicamentoUsuario.setUsuario(gerenciarLoginMB.getUsuario());
-		medicamentoService.salvaUsario(medicamentoUsuario);
+		medicamentoService.salvaMedicamentoUsuario(medicamentoUsuario);
 		this.inicializa();
 	}
 	
@@ -58,6 +61,32 @@ public class ManterUsoMedicamentoMB implements Serializable {
 	public boolean isAtualizacao(){
 		return medicamentoUsuario != null && medicamentoUsuario.getId() != null;
 	}
+	
+	
+	public List<Medicamento> completeMedicamento(String query){
+		if (medicamentos == null) 
+			medicamentos = medicamentoService.buscaMedicamentos(query, gerenciarLoginMB.getUsuario());
+
+		List<Medicamento> medicamentosBusca = new ArrayList<Medicamento>();
+         
+        for (int i = 0; i < medicamentos.size(); i++) {
+            Medicamento medicamento = medicamentos.get(i);
+            if(medicamento.getNome().trim().toLowerCase().startsWith(query)) {
+            	medicamentosBusca.add(medicamento);
+            }
+        }
+        return medicamentosBusca;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public MedicamentoUsuario getMedicamentoUsuario() {
 		return medicamentoUsuario;
